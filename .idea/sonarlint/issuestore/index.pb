0608f3bc7167acd733d7b931f23c4f130bb03258
Karl-Hendrik Veidenberg <karlhendrik01@gmail.com>
2021-03-14 20:45:10 +0200

    hello conflict

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+F
+GitHub hello world.iml,1\4\14573790a8a9faf2211d50900d42d0abdd0161b1
+C
+src/HelloWorld.java,f\8\f8f6ccab2f71fc7c0dd6b506e31cb856f7a2fa2d
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("george was here");
+        System.out.println("hello conflict");
     }
 }
